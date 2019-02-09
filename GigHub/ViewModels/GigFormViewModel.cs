6d63191aeeacb4937c318c5b4e485d902125eff7
Jakub Krejcirik <jakub.krejcirik@plus4u.net>
2019-02-09 21:22:39 +0100

    Implenet saving a gig.

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -8,7 +8,7 @@ namespace GigHub.ViewModels
         public string Venue { get; set; }
         public string Date { get; set; }
         public string Time { get; set; }
-        public int Genre { get; set; } /* int for storing id */
+        public byte Genre { get; set; } /* int for storing id */
         public IEnumerable<Genre> Genres { get; set; }
     }
 }
