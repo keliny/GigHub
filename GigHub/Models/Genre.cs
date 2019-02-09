bff1c6e7dedf4d183a1ba5f81dc071f1dcd2f476
Jakub Krejcirik <jakub.krejcirik@plus4u.net>
2019-02-09 15:30:19 +0100

    Create gig Model.

---
+++ GigHub/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; } /* byte is not nullable so it will be not null in db */
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+}
---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser Artist { get; set; } /* Users in DB derive from applicationUser, create artist */
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+    }
+}
